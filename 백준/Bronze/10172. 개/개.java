c204c279f77fb5c6f3ae41bebcf2d295f96268cf
kihyunnn <142724910+kihyunnn@users.noreply.github.com>
2024-08-15 23:40:17 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        long A = scanner.nextLong();
+//        long B = scanner.nextLong();
+//        long C = scanner.nextLong();
+//        System.out.println((A+B+C));
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
