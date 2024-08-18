8ca42c0599c0e92ab0d0544f03a3f3e39e28dbe4
김기현 <142724910+kihyunnn@users.noreply.github.com>
2024-08-19 01:56:44 +0900

    [Bronze V] Title: 아스키 코드, Time: 160 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //입력 받기 위한 스캐너 클래스
+
+        int n = sc.next().charAt(0);
+
+//        int [] arr = new int[5];
+//        int num = 0;
+//        int sum = 0;
+//        for(int i=0; i<arr.length; i++) {
+//            arr[i] = sc.nextInt();
+//        }
+
+        System.out.println(n);
+    }
+}
