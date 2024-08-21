3ce23db2b5f18de549083ee8bd391eca8f934cc8
김기현 <142724910+kihyunnn@users.noreply.github.com>
2024-08-22 01:47:13 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 164 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // 입력 받기 위한 스캐너 클래스
+
+        String str = sc.nextLine();
+        System.out.println(str.length());
+    }
+}
